97b69b9e3ad7f872a0ccb55898662ca1ead7c2cb
IvanMladenov <ismladenov@gmail.com>
2015-05-24 12:46:19 +0300

    Add new problems solved

---
+++ ExamPreparation/TextGravity/TextGravity.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextGravity
+{
+    class TextGravity
+    {
+        static void Main(string[] args)
+        {
+            int matrixCols = int.Parse(Console.ReadLine());
+            string sentence = Console.ReadLine();
+
+            int matrixRows;
+            if(sentence.Length%matrixCols==0)
+            {
+                matrixRows = sentence.Length / matrixCols;
+            }
+            else
+            {
+                matrixRows = sentence.Length / matrixCols + 1;
+            }
+            char[,] matrix = new char[matrixRows, matrixCols];
+
+            FillTheMatrix(sentence, matrix);
+
+            for (int row = matrix.GetLength(0)-2; row >= 0; row--)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix = Exchange(matrix, row, col);
+                }
+            }
+            Print(matrix);
+        }
+
+        static void Print(char[,]matrix)
+        {
+            Console.Write("<table>");//SecurityElement.Escape("<table>"));
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                Console.Write("<tr>");//"{0}", SecurityElement.Escape("<tr>"));
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write("<td>{0}</td>", matrix[row, col]);
+                }
+                Console.Write("</tr>");//"{0}", SecurityElement.Escape("</tr>"));
+            }
+            Console.WriteLine("</table>");//, SecurityElement.Escape("<table>"));
+        }
+
+        static char[,] Exchange(char[,]matrix,int currentRow, int currentCol)
+        {
+            if(matrix[currentRow+1,currentCol]==' ')
+            {
+                while(true)
+                {
+                    matrix[currentRow + 1, currentCol] = matrix[currentRow, currentCol];
+                    matrix[currentRow, currentCol] = ' ';
+                    currentRow++;
+                    if(currentRow+1>=matrix.GetLength(0)||matrix[currentRow+1,currentCol]!=' ')
+                    {
+                        break;
+                    }
+                }
+                //do
+                //{
+                //    matrix[currentRow + 1, currentCol] = matrix[currentRow, currentCol];
+                //    matrix[currentRow, currentCol] = ' ';
+                //    currentRow++;
+                //}
+                //while (matrix[currentRow+1,currentCol] == ' ' && currentRow < matrix.GetLength(0));
+            }
+            return matrix;
+        }
+
+        private static void FillTheMatrix(string sentence, char[,] matrix)
+        {
+            for (int row = 0, indexCounter = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++, indexCounter++)
+                {
+                    if (indexCounter < sentence.Length)
+                    {
+                        matrix[row, col] = sentence[indexCounter];
+                    }
+                    else
+                    {
+                        matrix[row, col] = ' ';
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ ExamPreparation/UppercaseWords/UppercaseWords.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UppercaseWords
+{
+    class UppercaseWords
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+            string pattern = @"^[A-Z]+(?=[^\w])|(?<=[^\w])[A-Z]+$|(?<=[^\w])[A-Z]+(?=[^\w])";
+            Regex reg = new Regex(pattern);
+            List<string> result = new List<string>();
+            while(!inputLine.Contains("END"))
+            {
+                MatchCollection matches = reg.Matches(inputLine);
+                HashSet<string> noRepeats = new HashSet<string>();
+                foreach(Match match in matches)
+                {
+                    noRepeats.Add(match.ToString());
+                }
+                foreach(string unique in noRepeats)
+                {
+                    string reversed = Reverse(unique);
+                    if (unique != reversed)
+                    {
+                        
+                        inputLine = inputLine.Replace(unique, reversed);
+                    }
+                    else
+                    {
+                        //inputLine = Regex.Replace(inputLine, unique, DoubleChars(unique));
+                        inputLine = inputLine.Replace(reversed, DoubleChars(reversed));
+                    }
+                }
+                result.Add(inputLine);
+                inputLine = Console.ReadLine();
+            }
+            foreach(string str in result)
+            {
+                Console.WriteLine(SecurityElement.Escape(str));
+            }
+        }
+
+        static string DoubleChars(string toDouble)
+        {
+            StringBuilder doubled = new StringBuilder();
+            for (int i = 0; i < toDouble.Length; i++)
+            {
+                doubled.Append(toDouble[i], 2);
+            }
+            return doubled.ToString();
+        }
+        
+        static string Reverse(string toReverse)
+        {
+            char[] arr = toReverse.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
