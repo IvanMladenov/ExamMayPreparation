ab8a49bc3654bf8f6fb9a7dd83288b961459aad5
IvanMladenov <ismladenov@gmail.com>
2015-05-27 17:56:48 +0300

    ne znam

--- ExamPreparation/SemanthicHTML/SemanthicHTML.cs
+++ ExamPreparation/SemanthicHTML/SemanthicHTML.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SemanthicHTML
 {
@@ -10,6 +11,37 @@ namespace SemanthicHTML
     {
         static void Main(string[] args)
         {
+            var row = Console.ReadLine();
+            var openTagPattern = @"<div(.*)(id|class)\s*=\s*""(\w+)""(.*)";
+            var closeTagPattern = @"<\/div>\s*<!--\s*(\w+)\s*-->";
+
+            while(row!="END")
+            {
+                if(Regex.IsMatch(row,openTagPattern))
+                {
+                    var matches = Regex.Match(row, openTagPattern);
+                    var tagName = matches.Groups[3].Value.Trim();
+                    var before=matches.Groups[1].Value.TrimEnd();
+                    var after = matches.Groups[4].Value.TrimEnd();
+                    var result = "<" + tagName + before + after;
+                    result.Replace("\\s+", " ");
+                    result.TrimEnd();
+                    Console.WriteLine(result);
+                }
+                else if(Regex.IsMatch(row,closeTagPattern))
+                {
+                    var matches = Regex.Match(row, closeTagPattern);
+                    var tagname =matches.Groups[1].Value.Trim();
+                    var result = "</" + tagname + ">";
+                    Console.WriteLine(result);
+
+                }
+                else
+                {
+                    Console.WriteLine(row);
+                }
+                row = Console.ReadLine();
+            }
         }
     }
 }
--- ExamPreparation/StuckNumbers/Program.cs
+++ ExamPreparation/StuckNumbers/Program.cs
@@ -10,6 +10,42 @@ namespace StuckNumbers
     {
         static void Main(string[] args)
         {
+            var count = Console.ReadLine();
+            var inputNumbers = Console.ReadLine();
+            var numbersArray = inputNumbers.Split(new char[]{' '},StringSplitOptions.RemoveEmptyEntries);
+            var output =new List<string>();
+
+            foreach(var a in numbersArray)
+            {
+                foreach (var b in numbersArray)
+                {
+                    foreach(var c in numbersArray)
+                    {
+                        foreach (var d in numbersArray)
+                        {
+                            if(a!=b&&a!=c&&a!=d&&b!=c&&b!=d&&c!=d)
+                            {
+                                if(a+b==c+d)
+                                {
+                                    output.Add((string.Format("{0}|{1}=={2}|{3}",a,b,c,d)));
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+            if(output.Count>0)
+            {
+                foreach(string item in output)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
         }
     }
 }
