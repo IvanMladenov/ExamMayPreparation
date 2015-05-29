c26c206ff401f5d68ff1f27bfecc2f3c4e07882a
IvanMladenov <ismladenov@gmail.com>
2015-05-29 11:00:23 +0300

    Matrix Shuffle for 100

--- ExamPreparation/MatrixShufle/MatrixShufle.cs
+++ ExamPreparation/MatrixShufle/MatrixShufle.cs
@@ -8,8 +8,8 @@ namespace MatrixShufle
 {
     class MatrixShufle
     {
-        public static int currentRow;
-        public static int currentCol;
+        //public static int currentRow;
+        //public static int currentCol;
         public static int matrixSize;
 
         static void Main(string[] args)
@@ -17,7 +17,7 @@ namespace MatrixShufle
             matrixSize = int.Parse(Console.ReadLine());
             List<char> textToFill = Console.ReadLine().ToList();
 
-            char[][]result=new char [matrixSize][];
+            char[,] result = new char[matrixSize, matrixSize];
 
             int textCount=textToFill.Count();
             for (int i =textCount ; i < matrixSize*matrixSize; i++)
@@ -25,52 +25,105 @@ namespace MatrixShufle
                 textToFill.Add(' ');
             }
 
-            currentCol = 0;
-            currentRow = 0;
+            SpiralMatrixFill(textToFill, result);
 
-            while(textToFill.Count>0)
+            StringBuilder whiteSquares = new StringBuilder();
+            StringBuilder blackSquares = new StringBuilder();
+            for (int row = 0; row < result.GetLength(0); row++)
             {
-                RightFill(result, textToFill);
-                DownFill(result, textToFill);
-                LeftFill(result, textToFill);
-                UpFill(result, textToFill);
+                if(row%2==0)
+                {
+                    whiteSquares.Append(WhiteOrBlackSquaresEstract(row, 0, result));
+                    blackSquares.Append(WhiteOrBlackSquaresEstract(row, 1, result));
+                }
+                else
+                {
+                    whiteSquares.Append(WhiteOrBlackSquaresEstract(row, 1, result));
+                    blackSquares.Append(WhiteOrBlackSquaresEstract(row, 0, result));
+                }
+            }
+
+            string final = whiteSquares.ToString() + blackSquares.ToString();
+
+            if(IsPalindrome(final))
+            {
+                Console.WriteLine("<div style='background-color:#4FE000'>{0} </div>", final);
+            }
+            else
+            {
+                Console.WriteLine("<div style='background-color:#E0000F'>{0} </div>", final);
             }
         }
 
-        static void RightFill(char[][] matrix,List<char>text)
+        static bool IsPalindrome(string toCheck)
         {
-            //char[][] filled = matrix;
-            int toStopAt=matrixSize-currentRow;
-            while(currentRow<toStopAt)
+            StringBuilder check = new StringBuilder();
+            toCheck = toCheck.ToLower();
+
+            for (int i = 0; i < toCheck.Length; i++)
             {
-                matrix[currentRow][currentCol] = text[0];
-                text.RemoveAt(0);
-                currentCol++;
+                if(char.IsLetter(toCheck[i]))
+                {
+                    check.Append(toCheck[i]);
+                }
             }
-                
-            //return filled;
+            string reversed = string.Join("", check.ToString().Reverse());
+            bool isPalindrome = reversed == check.ToString();
+            return isPalindrome;
         }
 
-        static char[][] LeftFill(char[][] matrix, List<char> text)
+        static string WhiteOrBlackSquaresEstract(int currRow, int startAt, char[,]matrix)
         {
-            char[][] filled = matrix;
-
-            return filled;
+            StringBuilder sb = new StringBuilder();
+            for (int i = startAt; i < matrix.GetLength(1); i+=2)
+            {
+                sb.Append(matrix[currRow, i]);
+            }
+            return sb.ToString();
         }
 
-        static char[][] DownFill(char[][] matrix, List<char> text)
+        private static void SpiralMatrixFill(List<char> textToFill, char[,] result)
         {
-            char[][] filled = matrix;
+            int firstLoopCol = 0; int firstLoopColEnd = matrixSize;
 
-            return filled;
+            int secondLoopRow = 1; int secondLoopRowEnd = matrixSize;
+
+            int thirdLoopRow = matrixSize - 1 - 1; int thirdLoopRowEnd = 0;
+
+            int fourthLoopRow = matrixSize - 1 - 1; int fourthLoopRowEnd = 1; int thirdLoopCol = 0;
+
+            while (textToFill.Count > 0)
+            {
+                for (int i = firstLoopCol; i < firstLoopColEnd; i++)
+                {
+                    result[firstLoopCol, i] = textToFill[0];
+                    textToFill.RemoveAt(0);
+                }
+                firstLoopCol++;
+                firstLoopColEnd--;
+                for (int i = secondLoopRow; i < secondLoopRowEnd; i++)
+                {
+                    result[i, secondLoopRowEnd - 1] = textToFill[0];
+                    textToFill.RemoveAt(0);
+                }
+                secondLoopRowEnd--;
+                secondLoopRow++;
+                for (int i = thirdLoopRow; i >= thirdLoopRowEnd; i--)
+                {
+                    result[thirdLoopRow + 1, i] = textToFill[0];
+                    textToFill.RemoveAt(0);
+                }
+                thirdLoopRow--;
+                thirdLoopRowEnd++;
+                for (int i = fourthLoopRow; i >= fourthLoopRowEnd; i--)
+                {
+                    result[i, thirdLoopCol] = textToFill[0];
+                    textToFill.RemoveAt(0);
+                }
+                thirdLoopCol++;
+                fourthLoopRowEnd++;
+                fourthLoopRow--;
+            }
         }
-
-        static char[][] UpFill(char[][] matrix, List<char> text)
-        {
-            char[][] filled = matrix;
-
-            return filled;
-        }
-
     }
 }
--- ExamPreparation/SumOfAllValues/SumOfAllValues.cs
+++ ExamPreparation/SumOfAllValues/SumOfAllValues.cs
@@ -13,11 +13,42 @@ namespace SumOfAllValues
         {
             string keyString = Console.ReadLine();
             string text = Console.ReadLine();
-            string keyPattern = @"^([a-zA-Z_]+)\d.+\d([a-zA-Z_]+)$";
+            string keyPattern = @"^([a-zA-Z_]+)\d.*\d([a-zA-Z_]+)$";//@"^([a-zA-Z_]+)\d.*\d([a-zA-Z_]+)$";
 
             Match match = Regex.Match(keyString, keyPattern);
+            if(match.Groups.Count<2)
+            {
+                Console.WriteLine("<p>A key is missing</p>");
+                return;
+            }
+
             string startKey = match.Groups[1].Value.ToString();
             string endKey = match.Groups[2].Value.ToString();
+            string decimalPattern = string.Format(@"{0}(\d*\.\d+){1}",startKey,endKey);
+            string integerPattern = string.Format(@"{0}(\d+){1}", startKey, endKey);
+            MatchCollection doubleMatches = Regex.Matches(text, decimalPattern);
+            MatchCollection intMathces = Regex.Matches(text, integerPattern);
+
+            double sum = 0;
+            foreach (Match item in doubleMatches)
+            {
+                double currentMatch = double.Parse(item.Groups[1].Value.ToString());
+                sum += currentMatch;
+            }
+
+            foreach (Match item in intMathces)
+            {
+                double current = double.Parse(item.Groups[1].Value.ToString());
+                sum += current;
+            }
+            if(sum!=0)
+            {
+                Console.WriteLine("<p>The total value is: <em>{0}</em></p>",sum);
+            }
+            else
+            {
+                Console.WriteLine("<p>The total value is: <em>nothing</em></p>");
+            }
         }
     }
 }
