4c2f2a4b46de489f97d17a1bf033f42b3b90e4a2
IvanMladenov <ismladenov@gmail.com>
2015-05-30 17:57:14 +0300

    Final Commit. Add Vladkos Notebook, and ITVillage.cs

---
+++ ExamPreparation/ITVillage/ITVillage.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITVillage
+{
+    class ITVillage
+    {
+        static void Main(string[] args)
+        {
+            string[] inputFields = Console.ReadLine().Split('|');
+            StringBuilder sb = new StringBuilder();
+            sb.Append(inputFields[0] + inputFields[1][7]+inputFields[2][7]+Reverse(inputFields[3])+inputFields[2][1]+inputFields[1][1]);
+            sb.Replace(" ", string.Empty);
+            string toCount=sb.ToString();
+
+            int[] startPos = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            int startIndex = StartPosition(startPos);
+
+            int[] dices = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+            int gameCoins = 50;
+            int innCounter = toCount.Count(x=>x=='I');
+            for (int i = 0; i < dices.Length; i++)
+            {
+                int currentMove = dices[i];
+                startIndex = (startIndex+currentMove)%sb.Length;
+                char currentChar = sb[startIndex];
+                if (currentChar == 'N')
+                {
+                    Console.WriteLine("<p>You won! Nakov's force was with you!<p>");
+                    return;
+                }
+                switch (currentChar)
+                {
+                    case 'P':
+                        gameCoins -= 5;
+                        break;
+
+                    case 'I':
+                        if (gameCoins >= 100)
+                        {
+                            gameCoins -= 100;
+                            innCounter--;
+                            if(innCounter==0)
+                            {
+                                Console.WriteLine("<p>You won! You own the village now! You have {0} coins!<p>",gameCoins);
+                                //return;
+                            }
+                        }
+                        else
+                        {
+                            gameCoins -= 10;
+                        }
+                        break;
+
+                    case 'F':
+                        gameCoins += 20;
+                        break;
+
+                    case 'S':
+                        i += 2;
+                        break;
+
+                    case 'V':
+                        gameCoins *= 10;
+                        break;
+                }
+              
+                if(gameCoins<0)
+                {
+                    Console.WriteLine("<p>You lost! You ran out of money!<p>");
+                    return;
+                }
+
+            }
+            Console.WriteLine("<p>You lost! No more moves! You have {0} coins!<p>",gameCoins);
+
+
+         }
+
+        static string Reverse(string x)
+        {
+            char[] arr = x.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+        static int StartPosition(int[] arr)
+        {
+            switch (arr[0])
+            {
+                case 1:
+                    return arr[1] - 1;
+                    break;
+                case 2:
+                    if (arr[1] == 1)
+                    {
+                        return 11;
+                    }
+                    return 4;
+                    break;
+                case 3:
+                    if (arr[1] == 1)
+                    {
+                        return 10;
+                    }
+                    return 5;
+                    break;
+                case 4:
+                    return 10 - arr[1];
+                    break;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
--- ExamPreparation/VladkosNotebook/Program.cs
+++ ExamPreparation/VladkosNotebook/Program.cs
@@ -10,45 +10,89 @@ namespace VladkosNotebook
     {
         static void Main(string[] args)
         {
-            string line = Console.ReadLine();
-            Dictionary<string, Player> pagesByColor=new Dictionary<string,Player>();
-
-            //pagesByColor["purple"]=new Player();
-            //pagesByColor[purple]=
-
-            while(!line.Contains("END"))
+            string entry = Console.ReadLine();
+            Dictionary<string, Player> pagesByColor = new Dictionary<string, Player>();
+            
+            while(!entry.Contains("END"))
             {
-                string[] data = line.Split('|');
+                string[] data = entry.Split('|');
                 string color = data[0];
                 if(!pagesByColor.ContainsKey(color))
                 {
                     pagesByColor[color] = new Player();
+                   // pagesByColor[color].Opponents = new List<string>();
                 }
-
+                Player currentPlayer = pagesByColor[color];
                 if(data[1]=="age")
                 {
-                    int age = int.Parse(data[2]);
-                    pagesByColor[color].Age = age;
+                    currentPlayer.Age = int.Parse(data[2]);
                 }
-
                 else if(data[1]=="name")
                 {
-                    pagesByColor[color].Name=data[2]
+                    currentPlayer.Name = data[2];
                 }
-                else if(data[1]=="win:)
+                else if(data[1]=="win")
                 {
-                      
+                    currentPlayer.WinCount++;
+                    currentPlayer.Opponents.Add(data[2]);
                 }
-                line = Console.ReadLine();
+                else if(data[1]=="loss")
+                {
+                    currentPlayer.LossCount++;
+                    currentPlayer.Opponents.Add(data[2]);
+                }
+                entry = Console.ReadLine();
             }
+
+            var validPages = pagesByColor                
+                .Where(p => p.Value.Age != 0&& p.Value.Name != null)
+                .OrderBy(p => p.Key);
+
+            if(validPages.Count()==0)
+            {
+                Console.WriteLine("No data recovered.");
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach(var page in validPages)
+            {
+                sb.AppendLine(string.Format("Color: {0}", page.Key));
+                sb.AppendLine(string.Format("-age: {0}", page.Value.Age));
+                sb.AppendLine(string.Format("-name: {0}", page.Value.Name));
+
+                string opponents = "(empty)";
+                if (page.Value.Opponents.Count > 0)
+                {
+                    var sortedOpponents = page.Value.Opponents.OrderBy(o => o, StringComparer.Ordinal);
+                    sb.AppendLine(string.Format("-opponents: {0}", string.Join(", ", sortedOpponents)));
+                }
+                else
+                {
+                    sb.AppendLine(string.Format("-opponents: {0}", opponents));
+                }
+                double rank = (double)(page.Value.WinCount + 1) / (page.Value.LossCount + 1);
+                sb.AppendLine(string.Format("-rank: {0:F2}",rank));
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
+
     class Player
     {
+        public Player()
+        {
+            this.Opponents = new List<string>();
+        }
+
         public string Name { get; set; }
+
         public int Age { get; set; }
-        public List<string> Oponents { get; set; }
-        public int Wins { get; set; }
-        public int Loss { get; set; }
+
+        public List<String> Opponents { get; set; }
+
+        public int WinCount { get; set; }
+
+        public int LossCount { get; set; }
     }
 }
