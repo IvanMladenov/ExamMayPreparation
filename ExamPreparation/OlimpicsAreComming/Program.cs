867d5fcb6931e128854ba3dbcbb0d06a8b672525
IvanMladenov <ismladenov@gmail.com>
2015-06-08 09:37:24 +0300

    The end

---
+++ ExamPreparation/OlimpicsAreComming/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace OlympicsAreComing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string,Country>all=new Dictionary<string,Country>();
+     
+            while (!input.Contains("report"))
+            {
+                string[] data = input.Split('|');
+                for (int i = 0; i < data.Length; i++)
+                {
+                    Regex reg = new Regex("\\s+");
+                    data[i] = reg.Replace(data[i], " ");
+                    data[i] = data[i].Trim();
+                }
+
+                if(!all.ContainsKey(data[1]))
+                {
+                    all.Add(data[1], new Country(data[0]));
+                    all[data[1]].Wins++;
+                }
+                else
+                {
+                    all[data[1]].Participants.Add(data[0]);
+                    all[data[1]].Wins++;
+                }
+               
+                input = Console.ReadLine();
+            }
+
+            var sorted = all.OrderByDescending(x => x.Value.Wins);
+
+            foreach (var item in sorted)
+            {
+                Console.WriteLine("{0} ({1} participants): {2} wins", item.Key, item.Value.Participants.Count(), item.Value.Wins);
+            }
+        }
+    }
+
+    class Country
+    {
+        public Country(string participant)
+        {
+            this.Participants = new HashSet<string>(){participant};
+        }
+
+        public int Wins { get; set; }
+
+        public HashSet<string> Participants { get; set; }
+    }
+}
---
+++ ExamPreparation/TargetPractice/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] RowAndCol = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            string snake = Console.ReadLine();
+            int[] shot = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+            char[,] matrix = new char[RowAndCol[0], RowAndCol[1]];
+
+            for (int row = RowAndCol[0] - 1, counter = 0; row >= 0; row --)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = RowAndCol[1] - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snake[counter];
+                        counter++;
+                        if (counter == snake.Length)
+                        {
+                            counter = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < RowAndCol[1]; col++)
+                    {
+                        matrix[row , col] = snake[counter];
+                        counter++;
+                        if (counter == snake.Length)
+                        {
+                            counter = 0;
+                        }
+                    }
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if(IsInside(row,col,shot))
+                    {
+                        matrix[row, col] = ' ';
+                    }
+                }
+            }
+
+            for (int row = matrix.GetLength(0) - 2; row >= 0; row--)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix = Exchange(matrix, row, col);
+                }
+            }
+
+            Print(matrix);
+
+        }
+
+        static char[,] Exchange(char[,] matrix, int currentRow, int currentCol)
+        {
+            if (matrix[currentRow + 1, currentCol] == ' ')
+            {
+                while (true)
+                {
+                    matrix[currentRow + 1, currentCol] = matrix[currentRow, currentCol];
+                    matrix[currentRow, currentCol] = ' ';
+                    currentRow++;
+                    if (currentRow + 1 >= matrix.GetLength(0) || matrix[currentRow + 1, currentCol] != ' ')
+                    {
+                        break;
+                    }
+                }
+                
+            }
+            return matrix;
+        }
+
+        static void Print(char[,]arr)
+        {
+            for (int row = 0; row < arr.GetLength(0); row++)
+			{
+                for (int col = 0; col < arr.GetLength(1); col++)
+                {
+                    Console.Write(arr[row, col]);
+                }
+                Console.WriteLine();
+			}
+        }
+
+        static bool IsInside(int pointX, int pointY, int[]centerAndRadius)
+        {
+            int centerX=centerAndRadius[0];
+            int centerY=centerAndRadius[1];
+            int radius = centerAndRadius[2];
+
+            bool isInCircle = (pointX - centerX) * (pointX - centerX) + (pointY - centerY) * (pointY - centerY) <= radius * radius;
+            return isInCircle;
+        }
+    }
+}
